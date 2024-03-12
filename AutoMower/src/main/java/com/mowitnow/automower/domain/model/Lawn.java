0da4b2b2e2eda4d1bdefd9895ef4d2747dddf506
AnassGaroual <50958582+AnassGaroual@users.noreply.github.com>
2024-03-12 03:54:32 +0100

    Implement & Refactor position within lawn logic

--- AutoMower/src/main/java/com/mowitnow/automower/domain/model/Lawn.java
+++ AutoMower/src/main/java/com/mowitnow/automower/domain/model/Lawn.java
@@ -3,7 +3,6 @@ package com.mowitnow.automower.domain.model;
 public record Lawn(int upperRightX, int upperRightY) {
 
 	public boolean isWithin(Position position) {
-		// TODO: Implement this method
-		throw new UnsupportedOperationException("Not yet implemented");
+		return (position.getX() <= upperRightX && position.getY() <= upperRightY);
 	}
 }
