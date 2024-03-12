71ff5ad27f6508bbfccc38f3ef64ef265149f312
AnassGaroual <50958582+AnassGaroual@users.noreply.github.com>
2024-03-12 03:36:33 +0100

    Add failing tests for orientation turn left method

---
+++ AutoMower/src/test/java/com/mowitnow/automower/domain/model/OrientationTest.java
@@ -0,0 +1,16 @@
+package com.mowitnow.automower.domain.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class OrientationTest {
+
+	@ParameterizedTest
+	@CsvSource({ "N, W", "W, S", "S, E", "E, N" })
+	void turnLeft_ShouldResultInCorrectOrientation(Orientation input, Orientation expected) {
+		assertEquals(expected, input.turnLeft());
+	}
+
+}
