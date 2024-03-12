a37e618346a105eb24630bfb2e9b7be44992fb5f
AnassGaroual <50958582+AnassGaroual@users.noreply.github.com>
2024-03-12 03:31:06 +0100

    Refactor positions creation logic

--- AutoMower/src/main/java/com/mowitnow/automower/domain/model/Position.java
+++ AutoMower/src/main/java/com/mowitnow/automower/domain/model/Position.java
@@ -8,13 +8,14 @@ import lombok.ToString;
 public final class Position {
 	private final int x;
 	private final int y;
+	private static final String COORDINATES_MUST_BE_POSITIVE = "Coordinates must be positive";
 
 	private Position(int x, int y) {
-		if (x >= 0 && y >= 0) {
-			this.x = x;
-			this.y = y;
-		} else
-			throw new IllegalArgumentException();
+		if (x < 0 || y < 0) {
+			throw new IllegalArgumentException(COORDINATES_MUST_BE_POSITIVE);
+		}
+		this.x = x;
+		this.y = y;
 	}
 
 	public static Position of(int x, int y) {
